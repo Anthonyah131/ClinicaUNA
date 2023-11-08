5bf7862c73e4283da2134fdee7fcc4c499e92198
Anthony Avila <105742243+Anthonyah131@users.noreply.github.com>
2023-11-07 18:44:33 -0600

    Correcion GuardarCita

--- ClinicaUNA/src/main/java/cr/ac/una/clinicauna/controller/P11_NuevaCitaViewController.java
+++ ClinicaUNA/src/main/java/cr/ac/una/clinicauna/controller/P11_NuevaCitaViewController.java
@@ -217,6 +217,9 @@ public class P11_NuevaCitaViewController extends Controller implements Initializ
     public void cargarDefecto(CliCitaDto cita, CliUsuarioDto usuario, CliAgendaDto agenda, CliMedicoDto medico, LocalDateTime fechaHora,
             CliCitaDto citasVec[], int pos) {
         citaDto = cita;
+        CliCitaService citaService = new CliCitaService();
+        Respuesta respuesta = citaService.getCita(citaDto.getCitId());
+        this.citaDto = (CliCitaDto) respuesta.getResultado("Cita");
         usuarioDto = usuario;
         agendaDto = agenda;
         medicoDto = medico;
